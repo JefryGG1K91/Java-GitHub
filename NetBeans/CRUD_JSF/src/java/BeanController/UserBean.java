04cae94f72c3e94c0deb492629800efa92759b40
Jefry A. Gutierrez <39036941+JefryGG1K91@users.noreply.github.com>
2019-02-01 22:12:24 -0400

    ADD uSER

--- NetBeans/CRUD_JSF/src/java/BeanController/UserBean.java
+++ NetBeans/CRUD_JSF/src/java/BeanController/UserBean.java
@@ -10,15 +10,21 @@ import Service.user.UserInterfaceLocal;
 import java.util.List;
 import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
 import javax.faces.bean.SessionScoped;
+import javax.faces.component.UIComponent;
+import javax.faces.component.UIInput;
+import javax.faces.context.FacesContext;
+import javax.faces.event.ComponentSystemEvent;
 
 /**
  *
  * @author jgutierrez
  */
 @ManagedBean(name = "userBean")
-@SessionScoped
+@RequestScoped
 public class UserBean {
 
     @EJB
@@ -30,30 +36,71 @@ public class UserBean {
     private String password1;
     private String password2;
     private List<Users> users;
-    
+
     @PostConstruct
-    public void init(){
-    
+    public void init() {
+
         this.users = userInterfaceLocal.getAllUsers();
-    
+
     }
-    
-//    public void addNewUser(){
-//    
-//    
-//    }
-//    
-//    public void updateUser(){
-//    
-//    
-//    }
-//    
-//    public void deleteUser(){
-//    
-//    
-//    }
-    
-    
+
+    public void addNewUser() {
+
+        Users userTemp = new Users();
+
+        userTemp.setFirstName(firstName);
+
+        userTemp.setLastName(lastName);
+
+        userTemp.setUserName(userName);
+
+        userTemp.setUserPassword(password1);
+
+        if (userInterfaceLocal.addUser(userTemp)) {
+
+          clearValues();
+
+        }
+
+    }
+
+    public void updateUser() {
+
+    }
+
+    public void deleteUser() {
+
+    }
+
+    public void validatePassword(ComponentSystemEvent event) {
+
+        FacesContext fc = FacesContext.getCurrentInstance();
+
+        UIComponent component = event.getComponent();
+
+        UIInput passOneInput = (UIInput) component.findComponent("passOne");
+        String passwordOne = passOneInput.getLocalValue() == null ? "" : passOneInput.getLocalValue().toString();
+        String passIdOne = passOneInput.getClientId();
+
+        UIInput uiInputConfirmPassword = (UIInput) component.findComponent("passTwo");
+        String confirmPassword = uiInputConfirmPassword.getLocalValue() == null ? ""
+                : uiInputConfirmPassword.getLocalValue().toString();
+
+        if (passwordOne.isEmpty() || confirmPassword.isEmpty()) {
+            return;
+        }
+
+        if (!passwordOne.equals(confirmPassword)) {
+
+            FacesMessage msg = new FacesMessage("Password must match confirm password");
+            msg.setSeverity(FacesMessage.SEVERITY_ERROR);
+            fc.addMessage(passIdOne, msg);
+            fc.renderResponse();
+
+        }
+
+    }
+
     public int getId() {
         return id;
     }
@@ -101,13 +148,21 @@ public class UserBean {
     public void setPassword1(String password) {
         this.password1 = password;
     }
-    
-     public String getPassword2() {
+
+    public String getPassword2() {
         return password2;
     }
 
     public void setPassword2(String password) {
         this.password2 = password;
     }
-    
+
+    public void clearValues() {
+            firstName = "";
+            lastName = "";
+            userName = "";
+            password1 = "";
+            password2 = "";
+    }
+
 }
